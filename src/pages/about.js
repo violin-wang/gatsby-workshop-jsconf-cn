5e0712bacbba817434cc2d8df1cd07cea990c8f8
Wang Lin <lin.wang6@wework.com>
2019-10-10 19:10:20 +0800

    feat(Pages): change to Layout

--- src/pages/about.js
+++ src/pages/about.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import { Link } from "gatsby";
 
+import Layout from '../components/Layout';
+
 export default () => (
-  <>
+  <Layout>
     <h1>关于我</h1>
     <p>这个是我的个人站点。</p>
     <Link to="/">&larr;返回首页</Link>
-  </>
+  </Layout>
 )
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,10 +1,12 @@
 import React from "react";
 import { Link } from "gatsby";
 
+import Layout from '../components/Layout';
+
 export default () => (
-  <>
+  <Layout>
     <h1>首页</h1>
     <p>Hello Shanghai!</p>
     <Link to="/about/">关于我&rarr;</Link>
-  </>
+  </Layout>
 );
