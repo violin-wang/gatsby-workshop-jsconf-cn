005d572f2ac3b170cc10e0dcc76394d0206ae444
Wang Lin <lin.wang6@wework.com>
2019-10-10 18:45:30 +0800

    feat(Pages): add about page

---
+++ src/pages/about.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import { Link } from "gatsby";
+
+export default () => (
+  <>
+    <h1>关于我</h1>
+    <p>这个是我的个人站点。</p>
+    <Link to="/">&larr;返回首页</Link>
+  </>
+)
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,3 +1,10 @@
 import React from "react";
+import { Link } from "gatsby";
 
-export default () => <div>Hello world!</div>;
+export default () => (
+  <>
+    <h1>首页</h1>
+    <p>Hello Shanghai!</p>
+    <Link to="/about/">关于我&rarr;</Link>
+  </>
+);
