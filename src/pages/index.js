038d0a49b530a779334c6c9f12be8978423892f0
Wang Lin <lin.wang6@wework.com>
2019-10-11 15:54:36 +0800

    feat(Home): add post list

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,12 +1,22 @@
 import React from "react";
 import { Link } from "gatsby";
 
-import Layout from '../components/Layout';
+import Layout from "../components/Layout";
+import PostPreview from "../components/PostPreview";
+import usePosts from "../hooks/usePosts";
 
-export default () => (
-  <Layout>
-    <h1>首页</h1>
-    <p>Hello Shanghai!</p>
-    <Link to="/about/">关于我&rarr;</Link>
-  </Layout>
-);
+export default () => {
+  const posts = usePosts();
+  return (
+    <Layout>
+      <h1>首页</h1>
+      <p>Hello Shanghai!</p>
+      <Link to="/about/">关于我&rarr;</Link>
+
+      <h2>我的博客</h2>
+      {posts.map(post => (
+        <PostPreview key={post.slug} post={post} />
+      ))}
+    </Layout>
+  );
+};
