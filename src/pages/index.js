b425c05f730fef9bb6ae49e27d5629c315cc4ad6
Wang Lin <lin.wang6@wework.com>
2019-10-11 16:16:49 +0800

    feat(Home): change to use Banner

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,22 +1,21 @@
 import React from "react";
-import { Link } from "gatsby";
 
 import Layout from "../components/Layout";
 import PostPreview from "../components/PostPreview";
 import usePosts from "../hooks/usePosts";
+import Banner from '../components/Banner';
 
 export default () => {
   const posts = usePosts();
   return (
-    <Layout>
-      <h1>首页</h1>
-      <p>Hello Shanghai!</p>
-      <Link to="/about/">关于我&rarr;</Link>
-
-      <h2>我的博客</h2>
-      {posts.map(post => (
-        <PostPreview key={post.slug} post={post} />
-      ))}
-    </Layout>
+    <>
+      <Banner />
+      <Layout>
+        <h2>我的博客</h2>
+        {posts.map(post => (
+          <PostPreview key={post.slug} post={post} />
+        ))}
+      </Layout>
+    </>
   );
 };
